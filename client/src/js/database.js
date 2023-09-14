517ca41fcd898be14c1b90d2c728d2a2aab2f75a
mengstrand <michael.d.engstrand1@gmail.com>
2023-09-14 07:33:52 -0500

    everything working on local, need to test heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,48 @@
 import { openDB } from 'idb';
 
+// Global constant
+const DB_NAME = "jate"
+
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB(DB_NAME, 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains(DB_NAME)) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore(DB_NAME, { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+
+  const theDb = await openDB(DB_NAME, 1);
+
+  const tx = theDb.transaction(DB_NAME, 'readwrite');
+
+
+  const storeVar = tx.objectStore(DB_NAME);
+
+  const request = storeVar.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
+
+export const getDb = async () => {
+
+  const theDb = await openDB(DB_NAME, 1);
+  const tx = theDb.transaction(DB_NAME, 'readonly');
+  const storeVar = tx.objectStore(DB_NAME);
+
+  const request = storeVar.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+
+  return result?.value;
+};
 
 initdb();
